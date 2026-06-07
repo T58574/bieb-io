a35f0a92eb6eb2feb117fdc806d5761a62713fe0
T58574 <lurker@ex.ra>
2026-06-08 02:30:03 +0300

    git rm --cached "*.bat"

--- commit-push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-git add .
-git commit -m %1
-if %errorlevel% neq 0 exit /b %errorlevel%
-git push
-if %errorlevel% neq 0 exit /b %errorlevel%
-exit /b 0
--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Starting Necro-Geometry...
-npm start
