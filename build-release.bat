7f0cdbfffb61920ac1ae77947b62169446a114bf
T58574 <lurker@ex.ra>
2026-06-08 00:03:33 +0300

    убрали весь хардкод + сделал генератор релиза, работает через флешку на любом пк

--- build-release.bat
+++ build-release.bat
@@ -1,18 +1,21 @@
 @echo off
+pushd "%~dp0"
 call .\test.bat
 if %errorlevel% neq 0 (
     echo [ERROR] Build failed
+    popd
     pause
     exit /b %errorlevel%
 )
-if exist release rmdir /s /q release
-mkdir release
-mkdir release\config
-mkdir release\client
-copy go-server\go-server.exe release\game.exe > nul
-xcopy /e /i /y config release\config > nul
-xcopy /e /i /y ts-client\dist release\client > nul
-echo @start game.exe > release\play.bat
+if exist "%~dp0release" rmdir /s /q "%~dp0release"
+mkdir "%~dp0release"
+mkdir "%~dp0release\config"
+mkdir "%~dp0release\client"
+copy "%~dp0go-server\go-server.exe" "%~dp0release\game.exe" > nul
+xcopy /e /i /y "%~dp0config" "%~dp0release\config" > nul
+xcopy /e /i /y "%~dp0ts-client\dist" "%~dp0release\client" > nul
+echo @start game.exe > "%~dp0release\play.bat"
 echo [SUCCESS] Release built in .\release
+popd
 pause
 exit /b 0
