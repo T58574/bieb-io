c20cec8372bee5de27b111aa0f7721db483470aa
T58574 <lurker@ex.ra>
2026-06-07 23:42:52 +0300

    fix: add pause to build-release.bat on success and failure

--- build-release.bat
+++ build-release.bat
@@ -1,6 +1,8 @@
 @echo off
 call .\test.bat
 if %errorlevel% neq 0 (
+    echo [ERROR] Build failed
+    pause
     exit /b %errorlevel%
 )
 if exist release rmdir /s /q release
@@ -11,4 +13,6 @@ copy go-server\go-server.exe release\game.exe > nul
 xcopy /e /i /y config release\config > nul
 xcopy /e /i /y ts-client\dist release\client > nul
 echo @start game.exe > release\play.bat
+echo [SUCCESS] Release built in .\release
+pause
 exit /b 0
