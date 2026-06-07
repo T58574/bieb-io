0794490e0739c1d57c7509cfb61a98310ce9f066
T58574 <lurker@ex.ra>
2026-06-07 19:18:38 +0300

    feat: add wave configuration file and remove legacy migration scripts

--- replace_collision.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open('go-server/game/collision.go', 'r') as f:
-    content = f.read()
-
-# Add killerID to triggerOnKillEffects so we can also drop loot there before the mob is removed. Wait, dropLoot is already called in updateMobs when mob dies.
-# If we want the killer's StatLootQuantity to affect dropLoot, we should store killerID in Mob struct when it takes fatal damage.
-pass
